d4b4fef342ab5bb60353d7b48a042c5f099ccb9d
Maija <maija.saisa@helsinki.fi>
2022-11-30 01:43:53 +0200

    add reference test

---
+++ src/tests/test_reference.py
@@ -0,0 +1,14 @@
+import unittest
+from entities.reference import Reference
+
+class TestReference(unittest.TestCase):
+    def setUp(self):
+        self.reference = Reference({"key": {
+            "author": "Kirjoittaja", 
+            "name": "Teoksen nimi", 
+            "year": "Julkaisuvuosi", 
+            "publisher": "Julkaisija"
+            }})
+    
+    def test_reference_initialization_works(self):
+        self.assertNotEqual(self.reference, None)
