b459c7d83e8d3af62ad4ae780f42a4eedf571bb7
vaisajuh <juha.vaisanen@gmail.com>
2022-12-14 18:00:52 +0200

    update tests

--- src/tests/references_test.py
+++ src/tests/references_test.py
@@ -38,7 +38,7 @@ class TestReferences(unittest.TestCase):
         
     def test_get_articles(self):
         ref.add_article(self.user_id, 'avain', 'kirjoittaja', 'otsikko', 'julkaisu', 2010, 5)
-        articles = ref.get_articles(self.user_id, 'avain')
+        articles = ref.get_articles(self.user_id, 'avain','')
         
         self.assertEqual(len(articles), 1)
         self.assertEqual(articles[0].title, 'otsikko')
@@ -46,7 +46,7 @@ class TestReferences(unittest.TestCase):
     
     def test_get_books(self):
         ref.add_book(self.user_id, 'avain', 'kirjoittaja', 'otsikko', 'julkaisija', 2010)
-        books = ref.get_books(self.user_id, 'avain')
+        books = ref.get_books(self.user_id, 'avain','')
         
         self.assertEqual(len(books), 1)
         self.assertEqual(books[0].title, 'otsikko')
@@ -54,7 +54,7 @@ class TestReferences(unittest.TestCase):
     
     def test_get_inproceedings(self):
         ref.add_inproceedings(self.user_id, 'avain', 'kirjoittaja', 'otsikko', 'kirjaotsikko', 2010)
-        inproceedings = ref.get_inproceedings(self.user_id, 'avain')
+        inproceedings = ref.get_inproceedings(self.user_id, 'avain','')
         
         self.assertEqual(len(inproceedings), 1)
         self.assertEqual(inproceedings[0].title, 'otsikko')
@@ -62,7 +62,7 @@ class TestReferences(unittest.TestCase):
     
     def test_get_masterthesis(self):
         ref.add_masterthesis(self.user_id, 'avain', 'kirjoittaja', 'otsikko', 'kirjaotsikko', 2010)
-        masterthesis = ref.get_master_thesis(self.user_id, 'avain')
+        masterthesis = ref.get_master_thesis(self.user_id, 'avain','')
         
         self.assertEqual(len(masterthesis), 1)
         self.assertEqual(masterthesis[0].title, 'otsikko')
@@ -76,10 +76,10 @@ class TestReferences(unittest.TestCase):
         
         ref.delete_all()
         
-        articles = ref.get_articles(self.user_id, "")
-        books = ref.get_books(self.user_id, "")
-        inproceedings = ref.get_inproceedings(self.user_id, "")
-        masterthesis = ref.get_master_thesis(self.user_id, "")
+        articles = ref.get_articles(self.user_id, '','')
+        books = ref.get_books(self.user_id, '','')
+        inproceedings = ref.get_inproceedings(self.user_id, '','')
+        masterthesis = ref.get_master_thesis(self.user_id, '','')
         
         self.assertEqual(len(articles), 0)
         self.assertEqual(len(books), 0)
